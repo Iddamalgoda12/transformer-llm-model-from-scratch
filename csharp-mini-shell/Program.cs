789ac58625d70ba43d0714cd240eab233cce7631
Iddamalgoda <piumanthashashimal@gmail.com>
2025-10-19 18:00:32 +0530

    added the loop,reads input,prints any input as "command not foud".

--- csharp-mini-shell/Program.cs
+++ csharp-mini-shell/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+while (true)
+{
+    Console.Write("$ ");
+
+    String? input = Console.ReadLine()?.Trim();
+    if (string.IsNullOrEmpty(input))
+    {
+        continue;
+    }
+    Console.WriteLine($"{input}: command not found");
+}
+
+
