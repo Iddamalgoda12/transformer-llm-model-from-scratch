d47490e19ba3728ab18c1c9c19d04d9d1c5b7ce5
Iddamalgoda <piumanthashashimal@gmail.com>
2025-10-19 18:08:49 +0530

    modified REPL,exit commands.

--- csharp-mini-shell/Program.cs
+++ csharp-mini-shell/Program.cs
@@ -9,7 +9,22 @@ while (true)
     {
         continue;
     }
+
+    string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    string command = parts[0].ToLower();
+
+    if (command == "exit")
+    {
+        int exitcode = 0;
+        if (parts.Length > 1 && int.TryParse(parts[1], out int code))
+        {
+            exitcode = code;
+        }
+        Environment.Exit(exitcode);
+
+    }
     Console.WriteLine($"{input}: command not found");
 }
+
 
 
