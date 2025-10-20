7ba773f820f71d1aa88c7ca9a7b67280724cd8cd
Iddamalgoda <piumanthashashimal@gmail.com>
2025-10-20 07:10:02 +0530

    added type command

--- csharp-mini-shell/Program.cs
+++ csharp-mini-shell/Program.cs
@@ -4,15 +4,19 @@ while (true)
 {
     Console.Write("$ ");
 
+    //checks the input for null also,removes spaces
     String? input = Console.ReadLine()?.Trim();
+
     if (string.IsNullOrEmpty(input))
     {
         continue;
     }
 
+    //devids input in to parts
     string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
     string command = parts[0].ToLower();
 
+    //exit codes added
     if (command == "exit")
     {
         int exitcode = 0;
@@ -24,22 +28,34 @@ while (true)
 
     }
     
-
+    //echo command
     if(command == "echo")
     {
         if(parts.Length>1)
         {
            Console.WriteLine(string.Join(' ', parts[1..]));
         }
-        else
-        {
-            Console.WriteLine();
-
-        }
         continue;
 
     }
-    Console.WriteLine($"{input}: command not found");
+    //added type command to find out buitin comands
+    if(command == "type")
+    {
+        if (parts.Length > 1)
+        {
+            if (parts[1] == "echo" || parts[1] == "exit" || parts[1] == "type")
+            {
+                Console.WriteLine($"{parts[1]}: is a shell builtin");
+            }
+
+            else
+            {
+                Console.WriteLine($"{string.Join(' ', parts[1..])}: command not found");
+
+            }
+        }
+            continue;
+    }
 }
 
 
